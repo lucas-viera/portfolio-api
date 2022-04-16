a5d4e871c145ca763c78218cbc40cfd28009b9ce
Lucas Viera <lucasviera22@gmail.com>
2022-04-16 12:03:15 -0300

    Added general structure to app.js or index.js file. Still must implement routers and middlewares

--- app.js
+++ app.js
@@ -1,36 +1,62 @@
-var express = require ('express');
-var app = express();
-var app_port = 3000;
+const express = require ('express');
+const app = express();
+const app_port = 3000;
+const bodyParser = require("body-parser");
+const path = require("path");
 
-// Root
-app.get('/', (request, response) =>{
-    response.sendStatus(200);
+// // Root
+// app.get('/', (request, response) =>{
+//     response.sendStatus(200);
+//     response.send("Visit to root");
+// });
+
+// // About
+// app.get('/about', (request, response) =>{
+//     response.sendStatus(200);
+//     response.send("Visit to about");
+// });
+
+// // Contact
+// app.get('/contact', (request, response) =>{
+//     response.sendStatus(200);
+// });
+
+// // Projects
+// app.get('/projects', (request, response) =>{
+//     response.sendStatus(200);
+// });
+
+// // Education
+// app.get('/education', (request, response) =>{
+//     response.sendStatus(200);
+// });
+
+// // Experience
+// app.get('/jobs', (request, response) =>{
+//     response.sendStatus(200);
+// });
+
+// Middlewares - requirements
+const loggerMiddleware = require("./middlewares/logger.middleware");
+const notFoundMiddleware = require("./middlewares/not-found.middleware");
+const errorsMiddleware = require("./middlewares/errors.middlware");
+// 
+app.use(loggerMiddleware);
+app.all("/*", notFoundMiddleware);
+app.use(errorsMiddleware);
+
+// Routers - requirements
+const authRouter = require("./routers/auth.router");
+const aboutRouter = require("./routers/about.router");
+const contactRouter = require("./routers/contact.router");
+const educationRouter = require("./routers/education.router");
+const experienceRouter = require("./routers/experience.router")
+const projectsRouter = require ("./routers/projects.router");
+
+//Others - errors
+app.all("/*", notFoundMiddleware);
+app.use(errorsMiddleware);
+
+var server = app.listen(app_port, ()=>{
+    console.log('Sever running on port', server.address().port)
 });
-
-// About
-app.get('/about', (request, response) =>{
-    response.sendStatus(200);
-});
-
-// Contact
-app.get('/contact', (request, response) =>{
-    response.sendStatus(200);
-});
-
-// Projects
-app.get('/projects', (request, response) =>{
-    response.sendStatus(200);
-});
-
-// Education
-app.get('/education', (request, response) =>{
-    response.sendStatus(200);
-});
-
-// Experience
-app.get('/jobs', (request, response) =>{
-    response.sendStatus(200);
-});
-
-
-app.listen(app_port);
