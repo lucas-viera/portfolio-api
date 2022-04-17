7292d2a9870e619a5e4492305064f5af5421ad09
Lucas Viera <lucasviera22@gmail.com>
2022-04-17 20:44:07 -0300

    Updated index.js file. Deleted comments. Added usage of routers and middlewares

--- src/index.js
+++ src/index.js
@@ -1,57 +1,42 @@
+const cors = require("cors");
 const express = require ('express');
 const app = express();
 const PORT = 3000;
 const bodyParser = require("body-parser");
 const path = require("path");
 
-// // Root
-// app.get('/', (request, response) =>{
-//     response.sendStatus(200);
-//     response.send("Visit to root");
-// });
-
-// // About
-// app.get('/about', (request, response) =>{
-//     response.sendStatus(200);
-//     response.send("Visit to about");
-// });
-
-// // Contact
-// app.get('/contact', (request, response) =>{
-//     response.sendStatus(200);
-// });
-
-// // Projects
-// app.get('/projects', (request, response) =>{
-//     response.sendStatus(200);
-// });
-
-// // Education
-// app.get('/education', (request, response) =>{
-//     response.sendStatus(200);
-// });
-
-// // Experience
-// app.get('/jobs', (request, response) =>{
-//     response.sendStatus(200);
-// });
+//Environment variables
+require("dotenv").config();
 
 // Middlewares - requirements
 const loggerMiddleware = require("./middlewares/logger.middleware");
 const notFoundMiddleware = require("./middlewares/not-found.middleware");
 const errorsMiddleware = require("./middlewares/errors.middlware");
-// 
-app.use(loggerMiddleware);
-app.all("/*", notFoundMiddleware);
-app.use(errorsMiddleware);
 
 // Routers - requirements
-const authRouter = require("./routers/auth.router");
+//const authRouter = require("./routers/auth.router");
 const aboutRouter = require("./routers/about.router");
 const contactRouter = require("./routers/contact.router");
 const educationRouter = require("./routers/education.router");
 const experienceRouter = require("./routers/experience.router")
 const projectsRouter = require ("./routers/projects.router");
+
+// Cors and external middlewares
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); 
+
+// Logger middleware must be used at the begining
+app.use(loggerMiddleware);
+app.all("/*", notFoundMiddleware);
+app.use(errorsMiddleware);
+
+// Routers - usage
+app.use("/about", aboutRouter);
+app.use("/contact", contactRouter);
+app.use("/education", educationRouter);
+app.use("/experience", experienceRouter);
+app.use("/projects", projectsRouter);
 
 //Others - errors
 app.all("/*", notFoundMiddleware);
